2bf135e31cff9065e6462b7b40a8c44e18b17c58
unknown <brporter@BPSPRO3.redmond.corp.microsoft.com>
2015-11-04 23:20:43 -0600

    Initial commit.

---
+++ src/svc/ImageGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BryanPorter.SlackMeme.Service
+{
+    // Generates our meme images
+    public class ImageGenerator
+    {
+        readonly static StringFormat TopFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Near };
+        readonly static StringFormat BtmFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Far };
+
+        const int StartingFontSize = 24;
+        const int FontSizeIncrement = 8;
+
+        public static Image GetImage(string rootPath, string imageKey, string topText, string bottomText)
+        {
+            var imagePath = System.IO.Path.Combine(rootPath, "images", string.Format("{0}.jpg", imageKey));
+
+            if (!System.IO.File.Exists(imagePath))
+                return null;
+
+            var img = Image.FromFile(imagePath);
+
+            using (var g = Graphics.FromImage(img))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                var topRectangle = new RectangleF(0, 0, img.Width, img.Height/2);
+                var bottomRectangle = new RectangleF(0, img.Height/2, img.Width, img.Height/2);
+                
+                var topPath = new GraphicsPath();
+                var btmPath = new GraphicsPath();
+
+                DrawString(g, topText.ToUpper(), topRectangle, TopFormat);
+                DrawString(g, bottomText.ToUpper(), bottomRectangle, BtmFormat);
+
+                g.Flush();
+            }
+
+            return img;
+        }
+
+        private static void DrawString(Graphics g, string text, RectangleF rectangle, StringFormat format)
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                SizeF size = SizeF.Empty;
+                Font font = null;
+
+                while (size.Height < rectangle.Size.Height &&
+                    size.Width < rectangle.Size.Width)
+                {
+                    font = font == null
+                        ? new Font(FontFamily.GenericSansSerif, StartingFontSize, FontStyle.Bold)
+                        : new Font(FontFamily.GenericSansSerif, font.Size + FontSizeIncrement, FontStyle.Bold);
+
+                    size = g.MeasureString(text, font, rectangle.Size, format);
+                }
+
+                var path = new GraphicsPath();
+                path.AddString(text, FontFamily.GenericSansSerif, (int)FontStyle.Bold, font.Size, rectangle, format);
+                g.DrawPath(Pens.Black, path);
+                g.FillPath(Brushes.White, path);
+            }
+        }
+    }
+}
---
+++ src/svc/ImageModule.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace BryanPorter.SlackMeme.Service
+{
+    public class ImageModule
+        : Nancy.NancyModule
+    {
+        const char LineSeperator = '\\';
+
+        public ImageModule(IRootPathProvider rootPathProvider)
+        {
+            Get["/image/{imageKey}"] = (parameters) =>
+            {
+                var text = Request.Query.text.HasValue ? (string)Request.Query.text : string.Empty;
+                string topText = string.Empty, bottomText = string.Empty;
+
+                if (text.IndexOf(LineSeperator) != -1)
+                {
+                    var parts = text.Split(LineSeperator);
+
+                    if (parts.Length > 1)
+                    {
+                        topText = parts[0];
+                        bottomText = parts[1];
+                    }
+                }
+                else
+                {
+                    topText = text;
+                }
+
+                var img = ImageGenerator.GetImage(rootPathProvider.GetRootPath(), parameters["imageKey"], topText,
+                    bottomText);
+
+                if (img != null)
+                {
+                    var ms = new MemoryStream();
+                    img.Save(ms, ImageFormat.Jpeg);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    return Response.FromStream(ms, "image/jpeg");
+                }
+
+                return HttpStatusCode.InternalServerError;
+            };
+
+            Post["/image/"] = _ =>
+            {
+                var text = Request.Form.text.HasValue ? Request.Form.text.ToString() : "dwight:Next+time\\say+something";
+                string[] parts = text.Split(':');
+                
+                if (parts.Length < 2)
+                {
+                    var result = new
+                    {
+                        text = "Sorry! I don't know what that means.",
+                        attachments = new[]
+                        {
+                            new
+                            {
+                                text =
+                                    "To generate a meme for the current channel, type '/meme <memetype>:<meme text>' and I'll generate and insert the meme for you.\nI know about the following memes:\n   * Success Kid (sk)\n   * All The Things (att)\n   * Dwight Schrute (dwight)\n   * I Don't Always (ida)\n   * Doge (doge)\n"
+                            }
+                        }
+                    };
+
+                    return Response.AsJson(result);
+                }
+
+                string imageKey = parts[0];
+                string memeText = parts[1];
+
+                var r = new
+                {
+                    response_type = "in_channel",
+                    text = "GENERATE ALL THE MEMES",
+                    attachments =
+                        new[]
+                        {
+                            new
+                            {
+                                image_url =
+                                    string.Format("https://bpslackmeme.azurewebsites.net/image/{0}?text={1}", imageKey,
+                                        Nancy.Helpers.HttpUtility.UrlEncode(memeText)),
+                                title = text
+                            }
+                        }
+                };
+                
+                return Response.AsJson(r, HttpStatusCode.OK);
+            };
+        }
+
+        private IEnumerable<string> GetImageKeys(string rootPath)
+        {
+            var imagesPath = Path.Combine(rootPath, "images");
+            var images = Directory.GetFiles(imagesPath, "*.jpg");
+
+            return images.Select(Path.GetFileNameWithoutExtension);
+        }
+    }
+}
