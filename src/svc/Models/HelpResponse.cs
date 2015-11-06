e8dbc5d0b5fd8b12dc1c2f3bffed3335bff5979a
unknown <brporter@BPSPRO3.redmond.corp.microsoft.com>
2015-11-05 23:14:22 -0600

    started integrating blob storage for generated images, added yoda to meme types

--- src/svc/Models/HelpResponse.cs
+++ src/svc/Models/HelpResponse.cs
@@ -13,7 +13,7 @@ namespace BryanPorter.SlackMeme.Service.Models
         {
             base.response_type = "ephemeral";
             base.text =
-                "To generate a meme for the current channel, type '/meme <memetype>:<meme text>' and I'll generate and insert the meme for you.\nI know about the following memes:\n   - Success Kid (sk)\n   - All The Things (att)\n   - Dwight Schrute (dwight)\n   - I Don't Always (ida)\n   - Doge (doge)\n";
+                "To generate a meme for the current channel, type '/meme <memetype>:<meme text>' and I'll generate and insert the meme for you.\nI know about the following memes:\n   - Success Kid (sk)\n   - All The Things (att)\n   - Dwight Schrute (dwight)\n   - I Don't Always (ida)\n   - Doge (doge)\n   - Yoda (yoda1)\n   - Thinkin' Yoda (yoda2)\n";
         }
     }
 }
