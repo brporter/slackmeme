fc3da892403140d5fc19015e0db0b385119c20a8
Bryan Porter <bryan@bryanporter.com>
2015-11-08 00:52:52 -0600

    fixed bug when input contains an escape sequence

--- src/svc/CommandParser.cs
+++ src/svc/CommandParser.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,7 +43,7 @@ namespace BryanPorter.SlackMeme.Service
                 parsedCommand = Command.HelpCommand;
                 return true;
             }
-            
+
             var text = string.Empty;
             var topLine = string.Empty;
             var bottomLine = string.Empty;
@@ -48,33 +52,73 @@ namespace BryanPorter.SlackMeme.Service
             var preamble = input.Substring(0, preambleSeperatorIndex);
             var textStartIndex = preambleSeperatorIndex + 1; // Don't include the seperator itself
 
-            if (preambleSeperatorIndex < input.Length)
+            if (textStartIndex < input.Length)
             {
                 // There is text after the preamble
                 text = input.Substring(
                     textStartIndex,
                     input.Length - (textStartIndex)
-                );
+                    );
+
+                // There may not be a second line; assign
+                // text block to topLine
+                topLine = text;
             }
 
-            var lineSeperatorIndex = input.IndexOf(LineSeperator);
+            var literal = text.ToLiteral();
+            var lines = literal.Split(new[] { LineSeperator }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (lineSeperatorIndex != -1)
-            {
-                // Input contains a line seperator.
-                topLine = text.Substring(0, lineSeperatorIndex);
-
-                var bottomLineStartIndex = lineSeperatorIndex + 1;
-
-                if (bottomLineStartIndex < text.Length)
-                {
-                    bottomLine = text.Substring(bottomLineStartIndex, text.Length - bottomLineStartIndex);
-                }
-            }
+            topLine = lines.Length >= 1 ? lines[0] : string.Empty;
+            bottomLine = lines.Length == 2 ? lines[1] : lines.Length > 2 ? string.Join("\\", lines.Skip(1)) : string.Empty;
 
             parsedCommand = new Command(preamble, topLine, bottomLine);
 
             return true;
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static string ToLiteral(this string input)
+        {
+            // The Horror: escape sequences in user-supplied input can't be evaluated without
+            // replacement in C#. Major pain when a user-supplied backslash literal is your
+            // line seperator. *sigh*
+
+            // http://stackoverflow.com/questions/323640/can-i-convert-a-c-sharp-string-value-to-an-escaped-string-literal
+
+            var literal = new StringBuilder(input.Length);
+
+            foreach (var c in input)
+            {
+                switch (c)
+                {
+                    case '\'': literal.Append(@"\'"); break;
+                    case '\"': literal.Append("\\\""); break;
+                    case '\\': literal.Append(@"\\"); break;
+                    case '\0': literal.Append(@"\0"); break;
+                    case '\a': literal.Append(@"\a"); break;
+                    case '\b': literal.Append(@"\b"); break;
+                    case '\f': literal.Append(@"\f"); break;
+                    case '\n': literal.Append(@"\n"); break;
+                    case '\r': literal.Append(@"\r"); break;
+                    case '\t': literal.Append(@"\t"); break;
+                    case '\v': literal.Append(@"\v"); break;
+                    default:
+                        if (Char.GetUnicodeCategory(c) != UnicodeCategory.Control)
+                        {
+                            literal.Append(c);
+                        }
+                        else
+                        {
+                            literal.Append(@"\u");
+                            literal.Append(((ushort)c).ToString("x4"));
+                        }
+                        break;
+                }
+            }
+
+            return literal.ToString();
         }
     }
 }
